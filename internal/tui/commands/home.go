a727b1a3eed4806e82d58335ee47189c639f9dcd
SimonVillalonIT <simonvillalon10@gmail.com>
2024-04-22 14:19:29 -0300

    Change list

---
+++ internal/tui/commands/home.go
@@ -0,0 +1,97 @@
+package cmds
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/DexterLB/mpvipc"
+	"github.com/SimonVillalonIT/music-golang/internal/services"
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/spf13/viper"
+)
+
+type MpvMsg bool
+type ConnMsg mpvipc.Connection
+type PlayMsg mpvipc.Connection
+
+type ErrMsg error
+type ConnErr error
+
+func StartMpv() tea.Msg {
+	if !isMpvRunning() {
+		err := exec.Command("mpv", "--no-video", "--idle", "--really-quiet", "--input-ipc-server=/tmp/mpvsocket").Run()
+
+		if err != nil {
+			log.Printf("Failed to start mpv: %v", err)
+
+			return ErrMsg(err)
+		}
+	}
+
+	return MpvMsg(true)
+}
+
+func isMpvRunning() bool {
+	cmd := exec.Command("ps", "-A")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	if err := cmd.Run(); err != nil {
+		log.Printf("Error checking for running processes: %v", err)
+		return false
+	}
+	return strings.Contains(out.String(), "mpv")
+}
+
+func SearchSongs() ([]services.Item, []list.Item, error) {
+	rawFile, err := os.ReadFile(viper.GetString(services.STORE_PATH))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var jsonFile []services.Item
+
+	err = json.Unmarshal(rawFile, &jsonFile)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	items := make([]list.Item, len(jsonFile))
+
+	for i, song := range jsonFile {
+		items[i] = song
+	}
+
+	return jsonFile, items, nil
+}
+
+func ConnCmd() tea.Msg {
+	conn := mpvipc.NewConnection("/tmp/mpvsocket")
+
+	err := conn.Open()
+
+	if conn.IsClosed() {
+		return ConnErr(err)
+	}
+
+	return ConnMsg(*conn)
+}
+
+func PlayCmd(conn mpvipc.Connection, item services.Item) tea.Cmd {
+	return func() tea.Msg {
+		err := services.Play(conn, item)
+
+		if err != nil {
+			return ErrMsg(err)
+		}
+		return PlayMsg(conn)
+	}
+}
+
+func kill() {
+	exec.Command("killall", "mpv").Run()
+}
---
+++ internal/tui/list/list.go
@@ -0,0 +1,153 @@
+package custom_list
+
+import (
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	appStyle = lipgloss.NewStyle().Padding(1, 2)
+
+	titleStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#FFFDF5")).
+			Background(lipgloss.Color("#25A065")).
+			Padding(0, 1)
+
+	statusMessageStyle = lipgloss.NewStyle().
+				Foreground(lipgloss.AdaptiveColor{Light: "#04B575", Dark: "#04B575"}).
+				Render
+)
+
+type listKeyMap struct {
+	toggleSpinner    key.Binding
+	toggleTitleBar   key.Binding
+	toggleStatusBar  key.Binding
+	togglePagination key.Binding
+	toggleHelpMenu   key.Binding
+	insertItem       key.Binding
+}
+
+func newListKeyMap() *listKeyMap {
+	return &listKeyMap{
+		insertItem: key.NewBinding(
+			key.WithKeys("a"),
+			key.WithHelp("a", "add item"),
+		),
+		toggleSpinner: key.NewBinding(
+			key.WithKeys("s"),
+			key.WithHelp("s", "toggle spinner"),
+		),
+		toggleTitleBar: key.NewBinding(
+			key.WithKeys("T"),
+			key.WithHelp("T", "toggle title"),
+		),
+		toggleStatusBar: key.NewBinding(
+			key.WithKeys("S"),
+			key.WithHelp("S", "toggle status"),
+		),
+		togglePagination: key.NewBinding(
+			key.WithKeys("P"),
+			key.WithHelp("P", "toggle pagination"),
+		),
+		toggleHelpMenu: key.NewBinding(
+			key.WithKeys("H"),
+			key.WithHelp("H", "toggle help"),
+		),
+	}
+}
+
+type Model struct {
+	list         list.Model
+	keys         *listKeyMap
+	delegateKeys *delegateKeyMap
+}
+
+func NewModel(items []list.Item) Model {
+	var (
+		delegateKeys = newDelegateKeyMap()
+		listKeys     = newListKeyMap()
+	)
+
+	delegate := newItemDelegate(delegateKeys)
+	musicList := list.New(items, delegate, 0, 0)
+	musicList.Title = "Songs & Playlists"
+	musicList.Styles.Title = titleStyle
+	musicList.AdditionalFullHelpKeys = func() []key.Binding {
+		return []key.Binding{
+			listKeys.toggleSpinner,
+			listKeys.insertItem,
+			listKeys.toggleTitleBar,
+			listKeys.toggleStatusBar,
+			listKeys.togglePagination,
+			listKeys.toggleHelpMenu,
+		}
+	}
+
+	return Model{
+		list:         musicList,
+		keys:         listKeys,
+		delegateKeys: delegateKeys,
+	}
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		h, v := appStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-h, msg.Height-v)
+
+	case tea.KeyMsg:
+		// Don't match any of the keys below if we're actively filtering.
+		if m.list.FilterState() == list.Filtering {
+			break
+		}
+
+		switch {
+		case key.Matches(msg, m.keys.toggleSpinner):
+			cmd := m.list.ToggleSpinner()
+			return m, cmd
+
+		case key.Matches(msg, m.keys.toggleTitleBar):
+			v := !m.list.ShowTitle()
+			m.list.SetShowTitle(v)
+			m.list.SetShowFilter(v)
+			m.list.SetFilteringEnabled(v)
+			return m, nil
+
+		case key.Matches(msg, m.keys.toggleStatusBar):
+			m.list.SetShowStatusBar(!m.list.ShowStatusBar())
+			return m, nil
+
+		case key.Matches(msg, m.keys.togglePagination):
+			m.list.SetShowPagination(!m.list.ShowPagination())
+			return m, nil
+
+		case key.Matches(msg, m.keys.toggleHelpMenu):
+			m.list.SetShowHelp(!m.list.ShowHelp())
+			return m, nil
+		}
+	}
+
+	// This will also call our delegate's update function.
+	newListModel, cmd := m.list.Update(msg)
+	m.list = newListModel
+	cmds = append(cmds, cmd)
+
+	return m, tea.Batch(cmds...)
+}
+
+func (m Model) View() string {
+	return appStyle.Render(m.list.View())
+}
+
+func (m Model) SelectedItem() list.Item {
+    return m.list.SelectedItem()
+}
