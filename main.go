5d0e21c433c5e3e010e689e015527526dc49bc15
SimonVillalonIT <simonvillalon10@gmail.com>
2024-05-07 08:31:11 -0300

    fix: remove log file

--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/SimonVillalonIT/go-music/cmd"
-	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	f, err := tea.LogToFile("debug.log", "debug")
-	if err != nil {
-		fmt.Println("fatal:", err)
-		os.Exit(1)
-	}
-	defer f.Close()
 	cmd.Execute()
 }
